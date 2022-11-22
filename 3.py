17f7c6c0f910433065823f38e64b6bcf8b1c8eea
N4N001 <N4N001@protonmail.com>
2022-11-22 15:31:28 +0100

    go to doděláno (asi)

--- 3.py
+++ 3.py
@@ -2,14 +2,13 @@ import turtle
 turtle.speed(50000)
 def ctverec(strana):
     for i in range(4):
+        turtle.pendown()
         turtle.fd(strana)
         turtle.lt(90)
+        turtle.penup()
 def pressebe(strana, pocet):
         for i in range(pocet):
             ctverec(strana)
-            turtle.penup()
-            x=turtle.pos()
-            turtle.goto(x[1]+strana / 2,x[2]+strana / 2)
-            turtle.pendown()
+            turtle.goto((i*strana / 2)+(strana / 2), (i*strana / 2)+(strana / 2))
 pressebe(int(input("Zadej délku strany: ")), int(input("Zadej počet čtverců: ")))
 turtle.done()
